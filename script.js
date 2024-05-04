521588f7054ac2706b0075211cc6f1e10e8b6f24
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-05-05 00:21:36 +0500

    updated variable names according to esp32.cpp

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function doPost(e) {
   // The Google SpreadSheet ID is "1hhUCOQtAi2QdW7ID2sm5YaQ1-0a46YPrn2F1G_SYycM" in this case
   // following this format https://docs.google.com/spreadsheets/d/spreadsheetId/edit#gid=0
   var sheet = SpreadsheetApp.openById(
-    "<Google SpreadSheet ID>"
+    "<YOUR_GOOGLE_SPREAD_SHEET_ID>"
   ).getActiveSheet();
   // Parse the JSON data
   var data = JSON.parse(e.postData.contents);
@@ -17,8 +17,8 @@ function doPost(e) {
     new Date(),
     data.temperature,
     data.humidity,
-    data.heat_index,
-    data.raw_sgp40,
+    data.heatIndex,
+    data.rawSGP40,
   ]);
   // Return success
   return ContentService.createTextOutput(
